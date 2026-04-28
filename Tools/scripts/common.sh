c2075340710eab4ba5e53c75c5c0a228999bc2d1
Andreas Atteneder <andreas.atteneder@unity3d.com>
2026-04-28 16:25:15 +0200

    chore: `Tools/scripts/reset-materials.sh` now covers some more previo… (#587)
    * chore: `Tools/scripts/reset-materials.sh` now covers some more previously missed files.
    
    * chore: Made sure path exploration is handled by Git so deleted files are included.

--- Tools/scripts/common.sh
+++ Tools/scripts/common.sh
@@ -5,10 +5,17 @@ reset_materials()
     echo "Resetting Materials"
     # (potentially altered during previous tests)
     pushd Packages/com.unity.cloud.gltfast.tests/Tests
-    git restore Runtime/Export/Materials/**/*.mat
-    git restore Resources/Export/Materials/**/*.mat
-    git restore Runtime/Export/ExportRenderTexture.renderTexture
-    git restore Runtime/RenderPipelineAssets/URP-Forward.asset
-    git restore Runtime/RenderPipelineAssets/URP-Forward_Renderer.asset
+    git restore 'Runtime/Export/Materials/**/*.mat'
+    git restore 'Resources/Export/Materials/**/*.mat'
+    git restore 'Runtime/Export/ExportRenderTexture.renderTexture'
+    git restore 'Runtime/RenderPipelineAssets/*.asset'
     popd
+
+    pushd Projects/glTFast-Test/Assets/
+    git restore 'Settings/*PipelineAsset.asset'
+    git restore 'Settings/*ForwardRenderer.asset'
+    git restore 'UniversalRenderPipelineGlobalSettings.asset'
+    popd
+
+    git restore 'Projects/glTFast-Test/ProjectSettings/GraphicsSettings.asset'
 }
--- Tools/scripts/reset-materials.sh
+++ Tools/scripts/reset-materials.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Resets changes to materials and render pipeline settings via git restore.
+#
+# When to use:
+# When testing with newer Unity versions URP/HDRP may auto-update many material files and you don't want to commit
+# those changes. With this script you can easily discard those changes without going through them manually.
+
 set -e
 
 pwd=$(cd -P -- "$(dirname -- "$0")" && printf '%s\n' "$(pwd -P)")
